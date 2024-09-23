52dfe80adebaaccc20ff9205ecdd7d613a14fe17
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-23 15:43:55 -0300

    up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # atividade-java
+
+como programar em java-script
---
+++ script.js
@@ -0,0 +1,69 @@
+// let, var, const -> tipagem dinamica
+
+// const const_var = 10// const não altera o valor nunca.
+
+// let valor = 10; //tipagem dinamica do js
+// console.log(typeof(valor), valor);
+
+// valor = "banana";
+// console.log(typeof(valor), valor);
+
+
+// let x = "teste let" // escopo local
+// var y = "teste var"// escopo global
+
+
+// funcao
+
+
+// function TesteLet() {
+//     if(true){
+//         let x = 10;
+//         console.log(x);
+//     }
+    
+
+// }
+
+// function TesteVar(){
+//     if(true){
+//         var y = "teste do var";
+//     }
+//     console.log(y);
+// }
+// TesteLet();
+// TesteVar();
+
+
+//funcão com parametro
+
+
+// function FuncComParams(x, y, z){
+
+// }   
+
+
+//objeto
+
+
+// let carro = {
+//     "Marca": "Mercedes",
+//     "Modelo": "C63 AMG",
+//     "Cor": "Azul",
+//     "Proprietario": {
+//         "Nome": "Marco",
+//         "Sobrenome": "Antonio"
+//     }    
+// }
+
+// console.log(carro);
+
+
+//array, matriz
+
+
+let arr = [1,2,3,4,"Marco", true, false, undefined, null];
+let mtx = [[123], [123], [1,2,3]];
+
+console.log(arr)
+console.log(mtx)
